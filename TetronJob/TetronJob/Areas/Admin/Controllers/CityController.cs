1cd59897e2797856b9f2b4abb0d311ae27928223
Mohammadreza <movahedijam@darnogroup.com>
2024-01-05 14:29:10 +0330

    Add City complate

--- TetronJob/TetronJob/Areas/Admin/Controllers/CityController.cs
+++ TetronJob/TetronJob/Areas/Admin/Controllers/CityController.cs
@@ -1,4 +1,7 @@
-﻿using MediatR;
+﻿using Application.Models;
+using Framework.ViewModels.City;
+using Framework.ViewModels.Province;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace TetronJob.Areas.Admin.Controllers
@@ -12,10 +15,97 @@ namespace TetronJob.Areas.Admin.Controllers
         {
             _mediator = mediator;
         }
-
-        public IActionResult Index()
+        [HttpGet]
+        public async Task<IActionResult> Index([FromQuery] PaginatedSearchWithSize options,Guid id)
         {
+            RequestCities request = new RequestCities()
+            {
+                Paginated = options,Id = id
+            };
+
+            var paginated = await _mediator.Send(request);
+            ViewBag.Province = id;
+            return View(paginated);
+        }
+        [HttpGet]
+        public IActionResult Create(Guid id)
+        {
+            ViewBag.Province = id;
             return View();
         }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Create(InsertCityViewModel model, CancellationToken cancellation)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _mediator.Send(model, cancellation);
+                if (result.IsSuccess == true)
+                {
+                    return RedirectToAction(nameof(Index),new{id=model.ProvinceId});
+                }
+                ViewBag.Alert = result.Message!;
+                return View(model);
+            }
+            ViewBag.Province = model.ProvinceId;
+            return View(model);
+        }
+
+
+
+        [HttpGet]
+
+        public async Task<IActionResult> Edit([FromRoute] Guid id)
+        {
+            var result = await _mediator.Send(new RequestGetCityById()
+            {
+                Id = id
+            });
+
+            return View(result);
+        }
+
+
+
+
+
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(UpdateCityViewModel model, CancellationToken cancellation)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _mediator.Send(model, cancellation);
+                if (result.IsSuccess == true)
+                {
+                    return RedirectToAction(nameof(Index), new { id = model.ProvinceId });
+                }
+                ViewBag.Alert = result.Message!;
+                return View(model);
+            }
+
+            return View(model);
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(Guid id,
+            CancellationToken cancellationToken)
+        {
+            var result = await _mediator.Send(new DeleteCityViewModel() { Id = id }, cancellationToken);
+
+            if (result.IsSuccess)
+                return Ok(result);
+
+            return BadRequest();
+        }
+
+
+
+
+
+
+
     }
 }
